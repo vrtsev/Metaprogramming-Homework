38a9170398162071bc8a136c06e85e9c450ef2c5
Vadim Ryazantsev <Vadym.Ryazantsev@SemitaSoft.com>
2017-01-16 16:58:57 +0200

    Implement custom struct-class

--- factory.rb
+++ factory.rb
@@ -1,3 +1,15 @@
 class Factory
+  def initialize(*args, &block)
+    args.each {|a| instance_variable_set("@#{a}", nil)}
+    self.class.class_eval(&block)
+  end
 
+  def new(*args)
+    args.each do |a|
+      instance_variables.each do |v|
+        instance_variable_set(v, a)
+      end
+    end
+    return self
+  end
 end
